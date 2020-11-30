0e5444537bf9f41bdd1696284ad1d6ee5f8ba883
cyr-il <56658128+cyr-il@users.noreply.github.com>
2020-11-30 14:02:32 +0100

    product page done

--- index.js
+++ index.js
@@ -68,4 +68,26 @@ $(document).ready(function(){
         }
     })
 
+    //product quantity section
+
+    let $qty_up = $('.qty .qty-up');
+    let $qty_down = $('.qty .qty-down');
+    let $input = $('.qty_input');
+
+    $qty_up.click(function(e) {
+        if($input.val() >= 1 && $input.val() <=9) {
+            $input.val(function(i,oldval) {
+                return ++oldval;
+            })
+        }
+    })
+
+    $qty_down.click(function(e) {
+        if($input.val() > 1 && $input.val() <=10) {
+            $input.val(function(i,oldval) {
+                return --oldval;
+            })
+        }
+    })
+
 });
