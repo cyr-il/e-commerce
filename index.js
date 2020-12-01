8ef57ec77cf45609a9d3432ff6478d5e4d2a3982
cyr-il <56658128+cyr-il@users.noreply.github.com>
2020-12-01 12:59:37 +0100

    ending cart.html

--- index.js
+++ index.js
@@ -72,9 +72,10 @@ $(document).ready(function(){
 
     let $qty_up = $('.qty .qty-up');
     let $qty_down = $('.qty .qty-down');
-    let $input = $('.qty_input');
+    //let $input = $('.qty_input');
 
     $qty_up.click(function(e) {
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
         if($input.val() >= 1 && $input.val() <=9) {
             $input.val(function(i,oldval) {
                 return ++oldval;
@@ -83,6 +84,7 @@ $(document).ready(function(){
     })
 
     $qty_down.click(function(e) {
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
         if($input.val() > 1 && $input.val() <=10) {
             $input.val(function(i,oldval) {
                 return --oldval;
